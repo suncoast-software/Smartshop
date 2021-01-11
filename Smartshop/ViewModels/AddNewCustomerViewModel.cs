c282ee374b9ec5d9ad30bc8f01eff6a7d9fc9b51
Suncoast Software <53012974+suncoast-software@users.noreply.github.com>
2021-01-11 17:27:58 -0500

    add NewCustomerView and ViewModel

---
+++ Smartshop/ViewModels/AddNewCustomerViewModel.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+using Smartshop.Data;
+using Smartshop.Models;
+using Smartshop.Utility;
+
+namespace Smartshop.ViewModels
+{
+   public class AddNewCustomerViewModel : BaseViewModel
+    {
+        public ICommand SaveCustomerCommand { get; set; }
+        public ICommand DeleteInputCommand { get; set; }
+
+        private Customer customer;
+        public Customer Customer
+        {
+            get { return customer; }
+            set { OnPropertyChanged(ref customer, value); }
+        }
+
+        public AddNewCustomerViewModel()
+        {
+            SaveCustomerCommand = new RelayCommand(SaveCustomer);
+            DeleteInputCommand = new RelayCommand(DeleteInputs);
+
+        }
+
+        public async void SaveCustomer()
+        {
+            using var db = new SmartshopDbContext();
+            await db.AddAsync(customer);
+            await db.SaveChangesAsync();
+        }
+
+        public void DeleteInputs()
+        {
+
+        }
+    }
+}
